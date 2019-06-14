75ca4e0df4d6fde238624f3dfba0892b8c94862c
im-maverick <muthukumar708@gmail.com>
2019-06-15 02:13:10 +0530

    Updates

---
+++ precache-manifest.597c4475484d2dc052093d59db04dcaf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a2ce42388461f1419fb8f75eb52237",
+    "url": "/PumpMonitor/static/media/slide_four.35a2ce42.jpg"
+  },
+  {
+    "revision": "c00997b9f89884482839",
+    "url": "/PumpMonitor/static/css/main.d591b120.chunk.css"
+  },
+  {
+    "revision": "26d07fb60be42c287e11",
+    "url": "/PumpMonitor/static/js/1.26d07fb6.chunk.js"
+  },
+  {
+    "revision": "5fb0b981fbf0ce4b25f6",
+    "url": "/PumpMonitor/static/js/runtime~main.5fb0b981.js"
+  },
+  {
+    "revision": "988649515544f535d630ac0dfe5701b9",
+    "url": "/PumpMonitor/static/media/slide_one.98864951.jpg"
+  },
+  {
+    "revision": "4c4fbd98096ab94e1b2af0035279b8d0",
+    "url": "/PumpMonitor/static/media/slide_three.4c4fbd98.jpg"
+  },
+  {
+    "revision": "c00997b9f89884482839",
+    "url": "/PumpMonitor/static/js/main.c00997b9.chunk.js"
+  },
+  {
+    "revision": "30a0dea54831088900e759b2405dc232",
+    "url": "/PumpMonitor/static/media/slide_five.30a0dea5.jpg"
+  },
+  {
+    "revision": "c8614f5bcf915d2a3c932e2b0e05320d",
+    "url": "/PumpMonitor/static/media/pic_one.c8614f5b.jpg"
+  },
+  {
+    "revision": "bfeb9a423130c90a257d8a9ea33e0fbb",
+    "url": "/PumpMonitor/static/media/pic_three.bfeb9a42.jpg"
+  },
+  {
+    "revision": "4285c52fa032f14d44af777e5a3d5ca4",
+    "url": "/PumpMonitor/static/media/pic_two.4285c52f.jpg"
+  },
+  {
+    "revision": "edf5a7842a4b161ecffed7220c422a7b",
+    "url": "/PumpMonitor/static/media/pic_four.edf5a784.jpg"
+  },
+  {
+    "revision": "4763fd39336f6f15ea75a43141a60c4c",
+    "url": "/PumpMonitor/index.html"
+  }
+];
